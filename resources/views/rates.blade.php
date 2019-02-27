a6f415de0b9f8ba5adc8eb71b47f810d6f47098d
kymuc <mkudimov1999@gmail.com>
2019-02-28 01:53:45 +0300

    set some behaviour to combobox

--- resources/views/rates.blade.php
+++ resources/views/rates.blade.php
@@ -5,15 +5,40 @@
     <div class="row justify-content-center" >
         <div class="col-md-8" >
             <div class="card">
-                <div class="card-header" style="background-color:#11255a;text-color:#ffffff"><select style="border:0px;outline:none;background-color:#11255a;color:#ffffff">
-                    <option selected value="{{$offices[0]->id}}">{{$offices[0]->adress}}</option>
-                    @for($i = 1; $i < count($offices); $i++)
+                <div class="card-header" style="background-color:#11255a;text-color:#ffffff">
+                    <form>
+                    <select name="exchange_office" style="border:0px;outline:none;background-color:#11255a;color:#ffffff" onchange="this.form.submit()">
+                    <option disabled selected>Select an exchange office</option>
+                    <!-- <option selected value="{{ $offices[0]->office_id }}">{{$offices[0]->adress}}</option> -->
+                    @for($i = 0; $i < count($offices); $i++)
                     <?php $office = $offices[$i]?>
-                    <option value = "{{$office->id}}">{{$office->adress}}</option>
+                    <option value = "{{ $office->office_id }}">{{$office->adress}}</option>
                     @endfor
                     </select>
+                    </form>
                 </div>
                 <div class="card-body">
+                    @if(isset($_GET["exchange_office"]))
+                        <?php
+                            $office_id = $_GET["exchange_office"];
+                            $exchange_rates = App\ExchangeRate::where('office_id', $office_id)->get();
+                        ?>
+                        <table class="card">
+                            <tr>
+                                <th>хуй</th>
+                                <th>жопа</th>
+                                <th>хех</th>
+                            </tr>    
+                        @for($i = 0; $i < count($exchange_rates); $i++)
+                        <?php $exchange_rate = $exchange_rates[$i] ?>
+                            <tr>
+                                <td>{{ App\Currency::where('currency_code', $exchange_rate->source_currency_code)->first()->currency_name }}</td>
+                                <td>{{ App\Currency::where('currency_code', $exchange_rate->target_currency_code)->first()->currency_name }}</td>
+                                <td>{{ number_format($exchange_rate->exchange_rate, 2) }}</td>
+                            </tr>    
+                        @endfor
+                        </table>
+                    @endif
                 </div>
             </div>
         </div>
